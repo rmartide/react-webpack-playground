c45dab2cef29f58562dc53bf8f83b5049233c5d9
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-03-19 05:06:30 +0100

    Cleaning css and changing class for hooks

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
-import React, { Component } from 'react';
 import Navbar from './Navbar';
-import GoogleLogin from 'react-google-login';
-import config from './conf.json';
-import axios from 'axios';
-export default class App extends Component {
+import React, { useState } from 'react'
 
-    constructor(props) {
-        super(props);
-    }
+export default function App() {
 
-    handle1 = () => {
-        rect.setAttribute("class", "rect2");
-    }
+    const [timesClicked, setTimesClicked] = useState(0)
 
-    handle2 = () => {
-        rect.setAttribute("class", "rect1");
-    }
-    copyToClipboard = (str) => {
-        navigator.clipboard.writeText(str);
-    }
-    responseGoogle = (response) => {
-        axios.get('/login', { headers: { 'x-auth': response.tokenId } }).then((result) => {
-            console.log(result);
-        }).catch((err) => {
-            console.log(err);
-        });
-        this.copyToClipboard(response.tokenId);
-    }
-    render() {
-        return (
-            <React.Fragment>
-                <Navbar />
-                <div className="al"></div>
-                <div className="container">
-                    <div className="header">Sign In</div>
-                    <div className='info'>*Click on the input boxes</div>
-                    <input id='username' className='text' onFocus={this.handle2} type="text" name="Username" placeholder='Username' />
-                    <input id='password' className='pass' onFocus={this.handle1} type="pass" name="Password" placeholder='Password' />
-                    <button>Sign In</button>
-                    <GoogleLogin
-                        clientId={config.clientId}
-                        buttonText="Login"
-                        onSuccess={this.responseGoogle}
-                        onFailure={this.responseGoogle}
-                    />
-                    <svg width="390" height="549" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                        <rect id='rect' className='rect1' x="45px" y="300px" rx="27" ry="27" width="300px" height="50px" style={{ stroke: '#fff', strokeWidth: '1px', fill: '#000' }} />
-                    </svg>
-                </div>
-            </React.Fragment>
-        )
-    }
+    return (
+        <>
+            <Navbar></Navbar>
+            <h1>Times clicked: {timesClicked}</h1>
+            <button onClick={() => setTimesClicked(timesClicked + 1)}>Click me!</button>
+        </>
+    )
 }
